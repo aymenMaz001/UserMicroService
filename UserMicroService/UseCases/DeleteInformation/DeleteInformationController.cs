f62d02070e3ce3e6bdf7d0f1e3846a73c1df25af
Aymen mazlout <aymen.mazlout@etu.unice.fr>
2020-10-02 03:35:53 +0100

    add controllers information

---
+++ UserMicroService/UseCases/DeleteInformation/DeleteInformationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserMicroService.UseCases.DeleteInformation
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeleteInformationController : ControllerBase
+    {
+        private readonly IUserWriteOnlyRepository _userWriteOnlyRepository;
+
+        public DeleteInformationController(IUserWriteOnlyRepository userWriteOnlyRepository)
+        {
+            _userWriteOnlyRepository = userWriteOnlyRepository;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            return Ok(_userWriteOnlyRepository.DeleteInformation(id).Result);
+        }
+    }
+}
---
+++ UserMicroService/UseCases/UpdateInformation/UpdateInformationController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Application.Repositories;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserMicroService.UseCases.UpdateInformation
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UpdateInformationController : ControllerBase
+    {
+        private readonly IUserWriteOnlyRepository _userWriteOnlyRepository;
+
+        public UpdateInformationController(IUserWriteOnlyRepository userWriteOnlyRepository)
+        {
+            _userWriteOnlyRepository = userWriteOnlyRepository;
+        }
+
+        // PUT: api/test/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct(int id, Information information)
+        {
+            return Ok(_userWriteOnlyRepository.UpdateInformation(id, information).Result);
+
+        }
+    }
+}
